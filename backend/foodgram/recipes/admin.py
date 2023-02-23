d3c9bddd9b4ed5ba607177b644969ec3638c3aac
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-23 22:42:02 +0300

    register Recipe model

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+# Регистрируем модель Recipe
+# сначала импортируем ее из нашего модуля models.py
+from .models import Recipe
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    # по ТЗ для модели рецептов в админ-зону в списке рецептов
+    # нужно вывести название рецепта и автора рецепта
+    list_display = ('pk', 'name', 'author')
+    # также по ТЗ нужно вывести фильтры по автору, названию рецепта и тегам
+    list_filter = ('author', 'name', 'tags')
+    # также по ТЗ нужно вывести общее кол-во добавлений этого рецепта
+    # в избранное
+
+
+# при регистрации модели Recipe источником конфигурации для нее
+# назначаем RecipeAdmin
+admin.site.register(Recipe, RecipeAdmin)
