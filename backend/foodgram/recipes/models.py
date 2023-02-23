f7e16d324a7b94d436cc22982d1110fc1a846ef8
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-23 19:21:13 +0300

    create model Recipes

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,3 +1,90 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Ingredients(models.Model):
+    """Модель ингредиентов."""
+    pass
+
+
+class Tags(models.Model):
+    """Модель тега."""
+    pass
+
+
+# объявляем класс Recipe - наследник класса Model
+# в соответствии с Django Coding Style поочередно выполняем след.шаги:
+# первый шаг - описываем поля модели, их типы, свойства
+# второй шаг - описываем класс Meta, чтобы добавить данные о самой модели
+# третий шаг - указываем метод __str__ - строковое представление объекта
+class Recipes(models.Model):
+    """Модель рецепта."""
+    # поле author поле отношения с моделью User
+    # указываем модель, связь с которой обеспечивает это поле - User
+    # указываем опцию on_delete со значением SET_NULL, то есть при удалении
+    # пользователя, во всех рецептах этого автора
+    # будет указан None или анонимный автор
+    # обязательно нужно указать null=True - то есть, допускается значение  NULL
+    author = models.ForeignKey(
+        User,
+        verbose_name='Автор рецепта',
+        related_name='recipes',
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    # поле представляет собой связь с таблицей Tags
+    tags = models.ManyToManyField(
+        Tags,
+        verbose_name='Теги',
+        related_name='recipes'
+    )
+
+    # поле представляет собой связь с таблицей Ingredients
+    ingredients = models.ManyToManyField(
+        Ingredients,
+        verbose_name='Ингридиенты',
+        related_name='recipes'
+    )
+
+    # текстовое поле с названием рецепта
+    # имеет ограничение по длине текста
+    name = models.CharField(
+        'Название рецепта',
+        max_length=200
+    )
+
+    # поле для изображения с рецептом
+    # укажем опцию upload_to -
+    # здесь указываем папку для сохранения файлов этого поля
+    # этот путь относительный и он продолжает путь указанный в MEDIA_ROOT
+    # то есть, путь будет '/media_root/recipes/'
+    image = models.ImageField(
+        'Изображение рецепта',
+        upload_to='recipes/'
+    )
+    # текстовое поле с описанием рецепта
+    # не имеет ограничения по длине текста
+    text = models.TextField(
+        'Описание рецепта'
+    )
+
+    # поле время приготовления должно быть положительным числовым значением
+    # для того чтобы проверить поле на соответствие минимальному значению
+    # используем встроенный валидатор
+    cooking_time = models.IntegerField(
+        'Время приготовления (мин)',
+        validators=(MinValueValidator(
+            1,
+            message='Минимально допустимое значение - 1 минута!'),)
+    )
+
+    class Meta:
+        verbose_name = 'Рецепт'
+        verbose_name_plural = 'Рецепты'
+        ordering = ['-id']
+
+    def __str__(self):
+        """Строковое представление объекта модели."""
+        return self.name
