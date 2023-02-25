2dcc44a220a56df6b5f237eb9eb402a1bee9d80e
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-25 18:02:48 +0300

    register model User in admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from .models import User
 # Register your models here.
+
+
+# регистрируем модель пользователя в админ-зоне
+
+class UserAdmin(admin.ModelAdmin):
+    # перечислим поля, которые должны отображаться в админке
+    list_display = ('email', 'username', 'first_name', 'last_name', 'password')
+    # по ТЗ нужно учесть фильтрацию по email и username
+    list_filter = ('email', 'username')
+    # также по ТЗ администратор должен иметь возможность
+    # менять пароль пользователю
+    list_editable = ('password',)
+
+
+admin.site.register(User, UserAdmin)
