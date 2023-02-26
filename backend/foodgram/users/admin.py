358ca498acf60aaa143c5cdace7d0f0ff505db61
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-26 21:53:18 +0300

    register Subscription model in admin

--- backend/foodgram/users/admin.py
+++ backend/foodgram/users/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Subscription
 # Register your models here.
 
 
 # регистрируем модель пользователя в админ-зоне
-
+@admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     # перечислим поля, которые должны отображаться в админке
     list_display = ('email', 'username', 'first_name', 'last_name', 'password')
@@ -12,8 +12,12 @@ class UserAdmin(admin.ModelAdmin):
     list_filter = ('email', 'username')
     # также по ТЗ администратор должен иметь возможность
     # менять пароль пользователю
+    search_fields = ('email', 'username')
     list_editable = ('password',)
     empty_value_display = '-пусто-'
 
 
-admin.site.register(User, UserAdmin)
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = ('user', 'author')
