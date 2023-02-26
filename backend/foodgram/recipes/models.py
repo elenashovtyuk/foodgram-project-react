477533d44436f587d0bda096e2c71ff5dc16de6c
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-02-26 15:34:10 +0300

    add ColorField, constants from settings

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,74 +1,71 @@
-from django.core.validators import MinValueValidator, RegexValidator
+from colorfield.fields import ColorField
+from django.core.validators import MinValueValidator
 from django.db import models
 from users.models import User
+from django.conf import settings
 
+# Модели приложения recipes:
 
-# объявляем класс Ingredient - наследник класса Model
-# в соответствии с Django Coding Style поочередно выполняем след.шаги:
+# В соответствии с Django Coding Style поочередно выполняем след.шаги:
 # первый шаг - описываем поля модели, их типы, свойства
 # второй шаг - описываем класс Meta, чтобы добавить данные о самой модели
 # третий шаг - указываем метод __str__ - строковое представление объекта
+
+
+# объявляем класс Ingredient - наследник класса Model
 class Ingredient(models.Model):
-    """Модель ингредиентов."""
+    """Модель описания ингредиентов для рецепта."""
     name = models.CharField(
         'Название ингредиента',
-        max_length=200
+        max_length=settings.MAX_LEN_FIELD,
     )
     measurement_unit = models.CharField(
         'Единица измерения',
-        max_length=200
+        max_length=settings.MAX_LEN_FIELD,
     )
 
     class Meta:
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
-        ordering = ['name']
+        ordering = ('name',)
 
     def __str__(self):
-        """Строковое представление объекта модели."""
-        return self.name
+        """Cтроковое представление объекта модели"""
+        return f'{self.name} ({self.measurement_unit})'
 
 
 # объявляем класс Tag - наследник класса Model
-# в соответствии с Django Coding Style поочередно выполняем след.шаги:
-# первый шаг - описываем поля модели, их типы, свойства
-# второй шаг - описываем класс Meta, чтобы добавить данные о самой модели
-# третий шаг - указываем метод __str__ - строковое представление объекта
 # все поля в модели Тег обязательны и уникальны
 class Tag(models.Model):
-    """Модель тега."""
+    """Модель описания тегов для рецептов."""
     name = models.CharField(
-        'Название',
+        'Название тега',
         unique=True,
-        max_length=200
+        max_length=settings.MAX_LEN_FIELD,
     )
-    # для поля color нужно применить валидатор, чтобы проверить,
-    # что введено корректное значение - цветовой код в нужном формате
-    # если в представленном пользователем значении не будет
-    # соответствующего - т.е в нужном формает, то вернется message
-    # с ошибкой
-    color = models.CharField(
+    # для поля color используем ColorField
+    # предварительно установив пакет django-colorfield
+    # и добавив модуль в settings.py/INSTALLED_APPS
+    color = ColorField(
         'Цветовой HEX-код',
+        format='hex',
         unique=True,
-        max_length=7,
-        validators=(
-            RegexValidator(
-                '^#([a-fA-F0-9]{6})',
-                message='Введенное значение не является цветовым HEX-кодом!'),)
+        max_length=settings.MAX_LEN_HEX_FIELD,
     )
     slug = models.SlugField(
-        'Уникальный слаг',
+        'Уникальный слаг тега',
         unique=True,
-        max_length=200
+        max_length=settings.MAX_LEN_FIELD,
     )
 
     class Meta:
         verbose_name = 'Тег'
         verbose_name_plural = 'Теги'
+        ordering = ('name',)
 
     def __str__(self):
         """Строковое представление объекта модели."""
-        return self.name
+        return f'{self.name} (цвет: {self.color})'
 
 
 # объявляем класс Recipe - наследник класса Model
@@ -77,7 +74,7 @@ class Tag(models.Model):
 # второй шаг - описываем класс Meta, чтобы добавить данные о самой модели
 # третий шаг - указываем метод __str__ - строковое представление объекта
 class Recipe(models.Model):
-    """Модель рецепта."""
+    """Модель описания рецепта."""
     # поле author поле отношения с моделью User
     # указываем модель, связь с которой обеспечивает это поле - User
     # указываем опцию on_delete со значением SET_NULL, то есть при удалении
@@ -89,14 +86,14 @@ class Recipe(models.Model):
         verbose_name='Автор рецепта',
         related_name='recipes',
         on_delete=models.SET_NULL,
-        null=True
+        null=True,
     )
 
     # поле представляет собой связь с таблицей Tags
     tags = models.ManyToManyField(
         Tag,
         verbose_name='Теги',
-        related_name='recipes'
+        related_name='recipes',
     )
 
     # поле представляет собой связь с таблицей Ingredients
@@ -111,14 +108,14 @@ class Recipe(models.Model):
         Ingredient,
         through='RecipeIngredient',
         verbose_name='Ингридиенты',
-        related_name='recipes'
+        related_name='recipes',
     )
 
     # текстовое поле с названием рецепта
     # имеет ограничение по длине текста
     name = models.CharField(
         'Название рецепта',
-        max_length=200
+        max_length=settings.MAX_LEN_FIELD,
     )
 
     # поле для изображения с рецептом
@@ -128,7 +125,7 @@ class Recipe(models.Model):
     # то есть, путь будет '/media_root/recipes/'
     image = models.ImageField(
         'Изображение рецепта',
-        upload_to='recipes/'
+        upload_to='recipes/',
     )
     # текстовое поле с описанием рецепта
     # не имеет ограничения по длине текста
@@ -141,19 +138,22 @@ class Recipe(models.Model):
     # используем встроенный валидатор
     cooking_time = models.IntegerField(
         'Время приготовления (мин)',
-        validators=(MinValueValidator(
-            1,
-            message='Минимально допустимое значение - 1 минута!'),)
+        validators=(
+          MinValueValidator(
+              settings.MIN_COOKING_TIME,
+              message='Минимально допустимое значение - 1 минута!',
+          ),
+        )
     )
 
     class Meta:
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
-        ordering = ['-id']
+        ordering = ('-id',)
 
     def __str__(self):
         """Строковое представление объекта модели."""
-        return self.name
+        return f'{self.name} (Автор: {self.author.username})'
 
 
 # объявляем класс RecipeIngredient -
@@ -162,26 +162,34 @@ class Recipe(models.Model):
 # ее нужно задавать "вручную", так как необходимы дополнительные поля
 # в промежуточной модели помимо тех, что автоматически создаются
 class RecipeIngredient(models.Model):
-    """Модель ингредиентов в составе рецепта"""
+    """
+    Модель для связи Recipe и Ingredient.
+    Содеражит сведения о количестве ингредиентов.
+    """
     # сначала явно указываем поля - внешние ключи от моделей,
     #  которые учавствуют в этой связи ManyToMany
     recipe = models.ForeignKey(
         Recipe,
         on_delete=models.CASCADE,
-        verbose_name='Рецепт',
-        related_name='recipes'
+        verbose_name='Связанный рецепт',
+        related_name='ingredients'
     )
     ingredient = models.ForeignKey(
         Ingredient,
         on_delete=models.CASCADE,
-        verbose_name='Ингредиент',
-        related_name='ingredients'
+        verbose_name='Связанные ингредиенты',
+        related_name='recipes'
     )
     # далее в этой промежуточной модели должно быть дополнительное
     # поле по кол-ву ингредиентов
     amount = models.PositiveSmallIntegerField(
         'Количество',
-        validators=[MinValueValidator(1, message='Минимальное количество 1!')])
+        validators=(
+            MinValueValidator(
+                settings.MIN_AMOUNT_INGR,
+                message='Минимальное количество 1!')
+        )
+    )
 
     # в настройках модели, в классе Meta нужно указать уникальность комбинации
     # ингредиент-рецепт
@@ -213,7 +221,10 @@ class RecipeIngredient(models.Model):
 # второй шаг - описываем класс Meta, чтобы добавить данные о самой модели
 # третий шаг - указываем метод __str__ - строковое представление объекта
 class Favorite(models.Model):
-    """Модель избранных рецептов."""
+    """
+    Модель связи Recipe и  User.
+    Содержит сведения об избранных рецептах пользователя.
+    """
     # 1-ое поле - пользователь, который добавил рецепт в Избраннное
     user = models.ForeignKey(
         User,
