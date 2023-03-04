d1536c4f3a61b1d4ce41094948bfda3303bb0c79
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-03-05 01:30:47 +0300

    add method create in CreateUserSerializer

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -74,6 +74,17 @@ class CreateUserSerializer(serializers.ModelSerializer):
     [POST] Сериализатор для модели пользователя(для записи).
     Создание нового пользователя.
     """
+    def create(self, validated_data):
+        user = User(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
     class Meta:
         model = User
         fields = (
