8bfd1586d3379c079a56936704b082de93c91fbe
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-03-15 15:18:15 +0300

    add custom filter

---
+++ backend/foodgram/api/filters.py
@@ -0,0 +1,12 @@
+from django_filters.rest_framework import FilterSet, filters
+from recipes.models import Recipe, Tag
+
+
+class RecipeFilter(FilterSet):
+    tags = filters.ModelMultipleChoiceFilter(field_name='tags__slug',
+                                             to_field_name='slug',
+                                             queryset=Tag.objects.all())
+
+    class Meta:
+        model = Recipe
+        fields = ('tags', 'author',)
