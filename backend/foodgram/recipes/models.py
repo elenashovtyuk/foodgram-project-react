a5a4812f0901853ef8db745294a9867b2af29a24
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-03-15 03:29:46 +0300

    change unique constraint

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -150,12 +150,12 @@ class Recipe(models.Model):
         verbose_name = 'Рецепт'
         verbose_name_plural = 'Рецепты'
         ordering = ('-id',)
-        # constraints = (
-        #     models.UniqueConstraint(
-        #         fields=('author', 'id'),
-        #         name='unique_recipe'
-        #     ),
-        # )
+        constraints = (
+            models.UniqueConstraint(
+                fields=('name',),
+                name='unique_recipe'
+            ),
+        )
 
     def __str__(self):
         """Строковое представление объекта модели."""
