963b7683a14131a45b398c59f430e01e5cad1a94
Elena Shovtyuk <110084590+elenashovtyuk@users.noreply.github.com>
2023-03-15 18:53:44 +0300

    delete my comments from scripts

--- backend/foodgram/scripts/load_ingredients.py
+++ backend/foodgram/scripts/load_ingredients.py
@@ -1,22 +1,11 @@
-# импортируем нужную модель для ингредиентов
-from recipes.models import Ingredient
-# импортируем модуль csv - модуль Python для чтения и записи файлов
 import csv
 
+from recipes.models import Ingredient
 
-# этот файл-скрипт должен реализовывать функцию run
-# это то, что запускается при запуске скрипта
+
 def run():
-    # открываем наш файл со списком ингредиентов
-    # из директории data/ingredients.csv как file.
-    # Для этого передаем переменную file в csv.reader
-    # метод reader модуля csv используется для извлечения данных
-    # в объект reader
     with open('data/ingredients.csv') as file:
         reader = csv.reader(file)
-        # next(reader)  # Advance past the header
-
-        # затем перебираем объект reader и извлекаем каждую строку наших данных
         for row in reader:
             ingredient = Ingredient(
                 name=row[0],
